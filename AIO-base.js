9526c7eef712dbe09963018a1a60ddebda2d9029
wangz <wangz@tiup.us>
2016-03-19 18:32:23 +0800

    获取某个页面上的偏移量

--- AIO-base.js
+++ AIO-base.js
@@ -223,3 +223,30 @@ function setInnerText(element, text){
 		element.innerText = text;
 	}
 }
+
+/*
+ * 获取某个元素在页面上的偏移量
+ */
+function getElementLeft(element){
+	var actualLeft = element.offsetLeft;
+	var current = element.offsetParent;
+
+	while(current !== null){
+		actualLeft += current.offsetLeft;
+		current = current.offsetParent;
+	}
+
+	return actualLeft;
+}
+
+function getElementTop(element){
+	var actualTop = element.offsetTop;
+	var current = element.offsetParent;
+
+	while(current !== null){
+		actualTop += current.offsetTop;
+		current = current.offsetParent;
+	}
+
+	return actualTop;
+}

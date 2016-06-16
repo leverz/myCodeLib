c26bf461e5377e1c66a3096b3a7752f853d93d72
zheng.wang <zheng.wang@renren-inc.com>
2016-06-16 21:27:48 +0800

    使用观察者模式

--- AIO-base.js
+++ AIO-base.js
@@ -779,8 +779,49 @@ _.arrayChunk = function (array, process, context) {
 	}, 100);
 };
 
+function EventTarget() {
+	this.handlers = {};
+}
 
+/**
+ * 观察者模式例子
+ * @type {{constructor: EventTarget, addHandler: EventTarget.addHandler, fire: EventTarget.fire, removeHandler: EventTarget.removeHandler}}
+ */
+EventTarget.prototype = {
+	constructor: EventTarget,
 
+	addHandler: function (type, handler) {
+		if(typeof this.handlers[type] === "undefined"){
+			this.handlers[type] = [];
+		}
+
+		this.handlers[type].push(handler);
+	},
+
+	fire: function (event) {
+		if(!event.target){
+			event.target = this;
+		}
+		if (this.handlers[event.type] instanceof Array){
+			var handlers = this.handlers[event.type];
+			for(var i = 0, len = handlers.length; i < len; i++){
+				handlers[i](event);
+			}
+		}
+	},
+
+	removeHandler: function (type, handler) {
+		if(this.handlers[type] instanceof Array){
+			var handlers = this.handlers[type];
+			for(var i = 0, len = handlers.length; i < len; i++){
+				if (handlers[i] === handler){
+					break;
+				}
+			}
+			handlers.splice(i, 1);
+		}
+	}
+};
 
 
 
